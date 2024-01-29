744007c2a8b804b19870398cd713f3a8da82335b
Seregno <bettale.alessandro@gmail.com>
2024-01-29 12:19:36 +0100

    Sostanzialmente nella pagina di login ho commentato tutto e inserito il test del login per vedere la risposta del server. Per tornare allo stato prima basta togliere tutto quello che non è commentato dalla pagina di login e poi scommentare il resto

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+/*'use client'
 import { getAntdFieldRequiredRule } from '@/app/helpers/validation'
 import { Button, Form } from 'antd'
 import React from 'react'
@@ -45,3 +45,95 @@ function LoginForm()
     )
 }
 export default Login
+*/
+
+'use client'
+import { getAntdFieldRequiredRule } from '@/app/helpers/validation'
+import { Button, Form } from 'antd'
+import React, { ChangeEvent } from 'react'
+import Router from 'next/router';
+import Image from 'next/image';
+import axios from 'axios';
+import {useState} from 'react';
+
+interface loginRequest
+{
+    email: string;
+    password: string;
+}
+
+function Login() {
+    return (
+        <div className="mainDiv">
+         
+        <Image src="/ci4_logo_homepage.png" alt='logo ci4' width={128} height={69} />
+        <br />
+        <LoginForm />
+      </div>
+    );
+  }
+  
+
+
+function LoginForm()
+{
+    //newAdds
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setPassword(e.target.value);
+    }
+
+    const handleLogin = async () => {
+        try{
+            const response = await axios.post('https://ci4.pesaventofilippo.com/api/v1/login', {    username: username,
+            password: password
+            
+        });
+        //https://ci4.pesaventofilippo.com/api/v1/login
+        console.log("Risposta del server: ",response.data);
+        }catch(error)
+        {
+            console.error("Errore durante il login: ",error);
+        }
+    };
+
+    //const onLog = (values: loginRequest) => {console.log(values)};
+    return (
+        <Form className = 'gap-5' itemID = 'loginForm' layout = 'vertical' /*onFinish={onLog}*/>
+            <Form.Item name = 'mail'  label = 'mail' rules = {[{required: true, message: 'inserire email'}]}>
+                <input type = 'email' />
+            </Form.Item>
+
+            <Form.Item name = 'password' label = 'password' rules = {[{required: true, message: 'inserire password'}]}>
+                <input type = 'password' />
+            </Form.Item>
+
+            <Button className ='loginBtn' type = 'primary' htmlType = 'submit' onClick={handleLogin} block>
+                Login
+            </Button>
+        </Form>
+    )
+}
+
+function LoggingIn()
+{
+        const onLog = async (values: loginRequest) => {
+            try{
+                const response = await axios.post('https://ci4.pesaventofilippo.com/api/v1/login', {    username: values.email,
+                password: values.password
+                
+            });
+            console.log(response.data);
+            }catch(error)
+            {
+                console.error(error);
+            }
+        };
+}
+export default Login;
--- src/app/helpers/validation.ts
+++
@@ -1,8 +0,0 @@
-export const getAntdFieldRequiredRule = (message: string) => {
-        return [
-        {
-        required: true,
-        message,
-        }
-    ]
-}
